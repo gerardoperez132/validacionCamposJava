7466e95e31e29cd26cd54bd20fec87acb30af691
Gerardo Perez <gerardo.perez132@gmail.com>
2013-08-29 17:02:12 -0430

    Nuevos métodos de validacion

--- src/utility/ValidacionCampos.java
+++ src/utility/ValidacionCampos.java
@@ -12,19 +12,41 @@ import java.util.regex.Pattern;
 */
 
 public class ValidacionCampos {
+
 	/**
 	* Metodo para validar que el campo solo tenga numeros de digitos del 0 al 9
 	*
 	*@version 1, 18/08/2013
-    *@author Gerardo Perez
+        *@author Gerardo Perez
 	*@param String campo a validad
-	*@return "True": Si lo que resibo no se encuentra en el patron de validacion "False": Si lo que resibo Se Encuntre dentro del patron de validacion
+	*@return <b>"True":</b> Si lo que resibo no se encuentra en el patron de validacion </br> <b>"False":</b> Si lo que resibo Se Encuntre dentro del patron de validacion
 	*/
 	public static boolean getSoloNumeros(String campo) {
 
 		Matcher matcher = null;	
 		Pattern fullRegex = Pattern.compile("[^0-9]+"); //Patron de validacion solo numeros
-		
+
+	    try {
+	    	matcher = fullRegex.matcher(campo); //Valido que los datos q recibo esten dentro del patron de validacion
+	    } catch (RuntimeException e) {
+	        System.out.println("Erro al validar campo");
+	    }
+		return matcher.find();
+	}
+
+	/**
+	* Metodo para validar que el campo solo tenga numeros de digitos del 0 al 9 y puntos
+	*
+	*@version 1, 18/08/2013
+        *@author Gerardo Perez
+	*@param String campo a validad
+	*@return <b>"True":</b> Si lo que resibo no se encuentra en el patron de validacion </br> <b>"False":</b> Si lo que resibo Se Encuntre dentro del patron de validacion
+	*/
+	public static boolean getSoloNumerosMasPunto(String campo) {
+
+		Matcher matcher = null;	
+		Pattern fullRegex = Pattern.compile("[^0-9\\.]+"); //Patron de validacion solo numeros mas puntos
+
 	    try {
 	    	matcher = fullRegex.matcher(campo); //Valido que los datos q recibo esten dentro del patron de validacion
 	    } catch (RuntimeException e) {
@@ -34,18 +56,18 @@ public class ValidacionCampos {
 	}
 	
 	/**
-	* Metodo para validar que el campo solo tenga numeros de digitos del 0 al 9 y puntos
+	* Metodo para validar que el campo solo tenga numeros de digitos del 0 al 9 y guiones
 	*
-	*@version 1, 18/08/2013
-    *@author Gerardo Perez
+	*@version 1, 29/08/2013
+        *@author Gerardo Perez
 	*@param String campo a validad
-	*@return "True": Si lo que resibo no se encuentra en el patron de validacion "False": Si lo que resibo Se Encuntre dentro del patron de validacion
+	*@return <b>"True":</b> Si lo que resibo no se encuentra en el patron de validacion </br> <b>"False":</b> Si lo que resibo Se Encuntre dentro del patron de validacion
 	*/
-	public static boolean getSoloNumerosMasPunto(String campo) {
+	public static boolean getSoloNumerosMasGuion(String campo) {
 
 		Matcher matcher = null;	
-		Pattern fullRegex = Pattern.compile("[^0-9\\.]+"); //Patron de validacion solo numeros mas puntos
-		
+		Pattern fullRegex = Pattern.compile("[^0-9\\-]+"); //Patron de validacion solo numeros mas puntos
+
 	    try {
 	    	matcher = fullRegex.matcher(campo); //Valido que los datos q recibo esten dentro del patron de validacion
 	    } catch (RuntimeException e) {
@@ -54,4 +76,42 @@ public class ValidacionCampos {
 		return matcher.find();
 	}
 	
+	/**
+	* Metodo para validar que el campo solo tenga numeros de digitos del 0 al 9, letras y guiones
+	*
+	*@version 1, 29/08/2013
+        *@author Gerardo Perez
+	*@param String campo a validad
+	*@return <b>"True":</b> Si lo que resibo no se encuentra en el patron de validacion </br> <b>"False":</b> Si lo que resibo Se Encuntre dentro del patron de validacion
+	*/
+	public static boolean getSoloNumerosLetrasMasGuion(String campo) {
+
+		Matcher matcher = null;
+		Pattern fullRegex = Pattern.compile("[^A-Za-z0-9\\-]+"); //Patron de validacion solo numeros mas puntos
+
+	    try {
+	    	matcher = fullRegex.matcher(campo); //Valido que los datos q recibo esten dentro del patron de validacion
+	    } catch (RuntimeException e) {
+	        System.out.println("Erro al validar campo");
+	    }
+		return matcher.find();
+	}
+	
+	/**
+	* Metodo para validar que el campo no este vacio
+	*
+	*@version 1, 29/08/2013
+        *@author Gerardo Perez
+	*@param String campo a validad
+	*@return <b>"True":</b> Si lo que resibo no tiene caracteres </br> <b>"False":</b> Si el campo tiene mas de un caracter
+	*/
+	public static boolean getCantidadCaracteresCero(String campo) {
+		
+		boolean b = false;
+		if(campo.toString().length() == 0 ){
+			b = true;
+		}
+		
+		return b;
+	}
 }
